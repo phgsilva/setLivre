9c470a6327754f025c31883d9b16cbf8420b81eb
Pedro Silva <ph.gsilva@hotmail.com>
2017-02-07 23:07:08 -0200

    ciracao dos models e realizacao do migrate do bd

--- setLivreApp/admin.py
+++ setLivreApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Artigo, Projeto, Integrante
 
 # Register your models here.
+
+admin.site.register(Artigo)
+admin.site.register(Projeto)
+admin.site.register(Integrante)
+
--- setLivreApp/models.py
+++ setLivreApp/models.py
@@ -1,5 +1,42 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Integrante(models.Model):
+    nome = models.CharField(max_length=100, blank=False)
+    bio = models.TextField()
+    caminho_foto = models.CharField(max_length=500)
+
+    def salvar(self):
+        self.save()
+
+    def __unicode__(self):
+        return 'Integrante Model' 
+
+class Projeto(models.Model):
+    nome = models.CharField(max_length=100, blank=False)
+    descricao = models.TextField()
+
+    def salvar(self):
+        self.save() 
+
+    def __unicode__(self):
+        return 'Projeto Model' 
+
+class Artigo(models.Model):
+    titulo = models.CharField(max_length=150, blank=False, null=False)
+    conteudo = models.TextField()
+    data_publicacao = models.DateTimeField()
+    caminho_foto = models.CharField(max_length=500)
+    usuario = models.ForeignKey('auth.User')
+    projeto = models.ForeignKey('Projeto')
+
+    def salvar(self):
+        self.data_publicacao = timezone.now()
+        self.save()
+
+    def __unicode__(self):
+        return 'Artigo Model' 
