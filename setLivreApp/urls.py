18aae96d6490aa6f5f3889ed1ad7a84dcb18fb47
Pedro Silva <ph.gsilva@hotmail.com>
2017-02-08 22:08:30 -0200

    implementdo view func e rotas basicas

--- setLivreApp/urls.py
+++ setLivreApp/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-
+    url(r'^$', views.index, name='index'),
+    url(r'^contato/$', views.contato, name='contato'),
+    url(r'^sobre/$', views.sobre, name='sobre'),
 ]
--- setLivreApp/views.py
+++ setLivreApp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'setLivreApp/index.html')
+
+def contato(request):
+    return render(request, 'setLivreApp/contato.html')
+
+def sobre(request):
+    return render(request, 'setLivreApp/sobre.html')
+
+def projeto(request, id):
+    return render(request, 'setLivreApp/projeto.html')
+
+def artigo(request, id):
+    return render(request, 'setLivreApp/artigo.html')
+
+def bio(request, id):
+    return render(request, 'setLivreApp/bio.html')
